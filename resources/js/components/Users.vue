33591d7abf94efd2228cc3d5efb0c8c3b81b334b
Jethro Hazelhurst <j.hazelhurst1994@gmail.com>
2019-12-19 23:04:06 +0000

    Fetch users from database using axios

--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -22,16 +22,20 @@
                       <th>ID</th>
                       <th>Name</th>
                       <th>Email</th>
+                      <th>Bio</th>
                       <th>Type</th>
+                      <th>Registered At</th>
                       <th>Modify</th>
                     </tr>
                   </thead>
                   <tbody>
-                    <tr>
-                      <td>183</td>
-                      <td>John Doe</td>
-                      <td>11-7-2014</td>
-                      <td><span class="tag tag-success">Approved</span></td>
+                    <tr v-for="user in users" :key="user.id">
+                      <td>{{ user.id }}</td>
+                      <td>{{ user.name }}</td>
+                      <td>{{ user.email }}</td>
+                      <td>{{ user.bio }}</td>
+                      <td>{{ user.type }}</td>
+                      <td>{{ user.created_at }}</td>
                       <td>
 
                           <a href="#">
@@ -126,6 +130,7 @@
     export default {
         data() {
             return {
+                users: {},
                 form: new Form({
                     name: '',
                     email: '',
@@ -138,13 +143,18 @@
         },
 
         methods: {
+            loadUsers(){
+                axios.get('api/user')
+                    .then(({ data }) => (this.users = data.data))
+            },
+
             createUser(){
                 this.form.post('api/user')
             }
         },
 
-        mounted() {
-            console.log()
+        created() {
+            this.loadUsers()
         }
     }
 </script>
