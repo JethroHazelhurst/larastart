a91ca7f8aca9fb621a56d5825b1307f7c2c39687
Jethro Hazelhurst <j.hazelhurst1994@gmail.com>
2019-12-21 14:40:32 +0000

    Add progress bar to add user component

--- resources/js/app.js
+++ resources/js/app.js
@@ -17,6 +17,22 @@ Vue.component(AlertError.name, AlertError)
 
 Vue.use(VueRouter)
 
+import VueProgressBar from 'vue-progressbar'
+const options = {
+  color: '#bffaf3',
+  failedColor: '#874b4b',
+  thickness: '5px',
+  transition: {
+    speed: '0.2s',
+    opacity: '0.6s',
+    termination: 300
+  },
+  autoRevert: true,
+  location: 'left',
+  inverse: false
+}
+
+Vue.use(VueProgressBar, options)
 
 const routes = [
   { path: '/dashboard', component: require('./components/Dashboard.vue').default },
--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -149,7 +149,9 @@
             },
 
             createUser(){
+                this.$Progress.start()
                 this.form.post('api/user')
+                this.$Progress.finish()
             }
         },
 
