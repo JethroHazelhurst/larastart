52b34f4b3414ce2d33b8c585d50732f477e8ab47
Jethro Hazelhurst <j.hazelhurst1994@gmail.com>
2020-01-02 19:37:43 +0000

    Allow multiple access levels

--- resources/js/Gate.js
+++ resources/js/Gate.js
@@ -1,13 +1,21 @@
-export default class Gate {
+export default class Gate{
     constructor(user){
         this.user = user;
     }
-
     isAdmin(){
         return this.user.type === 'admin';
     }
-
     isUser(){
         return this.user.type === 'user';
+    }
+    isAdminOrAuthor(){
+        if (this.user.type === 'admin' || this.user.type === 'author') {
+            return true;
+        }
+    }
+    isAuthorOrUser(){
+        if (this.user.type === 'user' || this.user.type === 'author') {
+            return true;
+        }
     }
 }
